6229147fb5fdcf48d52896cdadf423cc4f4f1eb9
Alessio Vertemati <alessio.vertemati@gmail.com>
2023-06-07 19:09:58 +0200

    Show processing status on document's page

--- resources/views/document/show.blade.php
+++ resources/views/document/show.blade.php
@@ -24,6 +24,15 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @if ($hasActivePipelines)
+                <div class="mb-4 bg-yellow-100 text-yellow-900 flex items-center gap-2 px-3 py-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                        <path fill-rule="evenodd" d="M15.312 11.424a5.5 5.5 0 01-9.201 2.466l-.312-.311h2.433a.75.75 0 000-1.5H3.989a.75.75 0 00-.75.75v4.242a.75.75 0 001.5 0v-2.43l.31.31a7 7 0 0011.712-3.138.75.75 0 00-1.449-.39zm1.23-3.723a.75.75 0 00.219-.53V2.929a.75.75 0 00-1.5 0V5.36l-.31-.31A7 7 0 003.239 8.188a.75.75 0 101.448.389A5.5 5.5 0 0113.89 6.11l.311.31h-2.432a.75.75 0 000 1.5h4.243a.75.75 0 00.53-.219z" clip-rule="evenodd" />
+                      </svg>
+                      
+                    <span class="">{{ __('Document under processing. Preview and search might not be available while the document is being processed.') }}</span>
+                </div>
+            @endif
             <div class="mb-4">
                 @if ($document->draft)
                     <span class="inline-block text-sm px-2 py-1 rounded-xl bg-gray-200 text-gray-900">{{ __('pending review') }}</span>
