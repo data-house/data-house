2507e2b8d5ae768b31d6c4594be243dabc209813
Alessio Vertemati <alessio.vertemati@gmail.com>
2023-07-11 21:37:10 +0200

    Add polling on root element

--- resources/views/livewire/current-question.blade.php
+++ resources/views/livewire/current-question.blade.php
@@ -1,8 +1,9 @@
-<div class="divide-y bg-lime-50" >
+<div class="divide-y bg-lime-50" wire:poll.visible="">
 
     @if ($question && $question->isPending())
         
-        <x-question :poll="true" :id="$ref ?? null" :question="$question" />
+        <x-question :id="$ref ?? null" :question="$question" />
     
     @endif
+    
 </div>
