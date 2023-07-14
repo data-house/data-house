2a19cb8e9f4da1ab8820242595a19a4a77ff1a0f
Alessio Vertemati <alessio.vertemati@gmail.com>
2023-07-14 12:22:31 +0200

    Show login button on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,5 +5,11 @@
         </x-slot>
 
         <h1 class="text-3xl font-bold text-center">{{ config('app.name') }}</h1>
+
+        <div class="mt-8 text-center">
+            <x-button-link href="{{ route('login') }}">
+                {{ __('Log in') }}
+            </x-button-link>
+        </div>
     </x-authentication-card>
 </x-guest-layout>
