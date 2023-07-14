50f07c00467f09600424b3cad056716e4433ea48
Alessio Vertemati <alessio.vertemati@gmail.com>
2023-07-14 21:02:48 +0200

    Add copy to clipboard plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,12 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
+import Clipboard from "@ryangjchandler/alpine-clipboard";
+
 window.Alpine = Alpine;
 
 Alpine.plugin(focus);
+
+Alpine.plugin(Clipboard);
 
 Alpine.start();
