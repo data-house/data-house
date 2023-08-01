da577ff4dde07fd0c3c09ce0fbf314e34dd7f503
Alessio Vertemati <alessio.vertemati@gmail.com>
2023-08-01 17:47:24 +0200

    Define geographic areas based on UN M49

---
+++ app/Models/GeographicRegion.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Collection;
+
+class GeographicRegion
+{
+    private static $countries;
+    
+    public static function all()
+    {
+        if(!is_null(static::$countries)){
+            return static::$countries;
+        }
+
+        $file = file_get_contents(resource_path('data/geographic-regions.json'));
+
+        static::$countries = json_decode($file, true);
+
+        return static::$countries;
+    }
+
+
+
+    /**
+     * Get the geographic regions of the given countries 
+     */
+    public static function from(Collection $countryCodes): Collection
+    {
+        static::all();
+
+        return $countryCodes?->map(fn($c) => static::$countries[$c])
+            ->filter()
+            ->map(fn($e) => collect($e)->only([
+                "region-name",
+                "sub-region-name",
+                "intermediate-region-name",
+            ])->values())
+            ->flatten()
+            ->filter();
+    }
+}
---
+++ tests/Feature/GeographicRegionTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\GeographicRegion;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class GeographicRegionTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    public function test_regions_can_be_loaded(): void
+    {
+        $regions = GeographicRegion::all();
+
+        $this->assertCount(248, $regions);
+
+        $this->assertEquals([
+            "region-code" => 150,
+            "region-name" => "Europe",
+            "sub-region-code" => 155,
+            "sub-region-name" => "Western Europe",
+            "intermediate-region-code" => null,
+            "intermediate-region-name" => null,
+            "country-or-area" => "Germany",
+            "M49-code" => 276,
+            "iso-alpha2" => "DE",
+            "iso-alpha3" => "DEU"
+        ], $regions['DEU']);
+        
+        $this->assertFalse($regions['DE'] ?? false);
+    }
+
+    public function test_all_regions_returned_for_country()
+    {
+        $regions = GeographicRegion::from(collect(['DEU']));
+
+        $this->assertEquals([
+            "Europe",
+            "Western Europe",
+        ], $regions->toArray());
+        
+        $regions = GeographicRegion::from(collect(['CRI']));
+
+        $this->assertEquals([
+            "Americas",
+            "Latin America and the Caribbean",
+            "Central America",
+        ], $regions->toArray());
+        
+    }
+}
