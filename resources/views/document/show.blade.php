89438cbdf2735eb173abdb8b75cf7711db757c3c
Alessio Vertemati <alessio.vertemati@gmail.com>
2023-08-01 17:50:53 +0200

    Ensure document loads even if no project is connected

--- resources/views/document/show.blade.php
+++ resources/views/document/show.blade.php
@@ -70,36 +70,40 @@
                     <div class="space-y-2">
                         <h4 class="font-bold">{{ __('Project') }}</h4>
                         
-                        <div>{{ $document->project?->title }}</div>
-                        <div>
-                            <p class="text-xs uppercase block text-stone-700">{{ __('Topics') }}</p>
-                            @foreach ($document->project?->topics as $topic)
-                                <span class="flex gap-2 items-center text-sm px-2 py-1 rounded-xl bg-gray-200 text-gray-900">
-                                    <x-heroicon-o-hashtag class="w-5 h-5" />
-                                    {{ $topic }}
-                                </span>
-                            @endforeach
-                        </div>
-                        <div>
-                            <p class="text-xs uppercase block text-stone-700">{{ __('Countries') }}</p>
-                            <div class="prose">
-                                <ul>
-                                    @foreach ($document->project?->countries()->pluck('value') as $country)
-                                        <li>{{ $country }}</li>
-                                   @endforeach 
-                                </ul>
+                        @if ($document->project)
+                            <div>{{ $document->project?->title }}</div>
+                            <div>
+                                <p class="text-xs uppercase block text-stone-700">{{ __('Topics') }}</p>
+                                @foreach ($document->project?->topics as $topic)
+                                    <span class="flex gap-2 items-center text-sm px-2 py-1 rounded-xl bg-gray-200 text-gray-900">
+                                        <x-heroicon-o-hashtag class="w-5 h-5" />
+                                        {{ $topic }}
+                                    </span>
+                                @endforeach
                             </div>
-                        </div>
-                        <div>
-                            <p class="text-xs uppercase block text-stone-700">{{ __('Regions') }}</p>
-                            <div class="prose">
-                                <ul>
-                                    @foreach ($document->project?->regions() as $region)
-                                        <li>{{ $region }}</li>
-                                   @endforeach 
-                                </ul>
+                            <div>
+                                <p class="text-xs uppercase block text-stone-700">{{ __('Countries') }}</p>
+                                <div class="prose">
+                                    <ul>
+                                        @foreach ($document->project?->countries()->pluck('value') as $country)
+                                            <li>{{ $country }}</li>
+                                    @endforeach 
+                                    </ul>
+                                </div>
                             </div>
-                        </div>
+                            <div>
+                                <p class="text-xs uppercase block text-stone-700">{{ __('Regions') }}</p>
+                                <div class="prose">
+                                    <ul>
+                                        @foreach ($document->project?->regions() as $region)
+                                            <li>{{ $region }}</li>
+                                    @endforeach 
+                                    </ul>
+                                </div>
+                            </div>
+                        @else
+                            <p class="prose">{{ __('Project not identified') }}</p>
+                        @endif
                     </div>
 
                     <div class="space-y-2">
