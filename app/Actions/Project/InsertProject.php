fe5e9efb7b2dd5435ed9940c51578167b81ece46
Alessio Vertemati <alessio.vertemati@gmail.com>
2023-08-04 14:13:12 +0200

    Action to create a project

---
+++ app/Actions/Project/InsertProject.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Actions\Project;
+
+use App\Models\Project;
+use App\Models\ProjectType;
+use App\Models\User;
+use App\Models\Visibility;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Enum;
+use PrinsFrank\Standards\Country\CountryAlpha3;
+
+class InsertProject
+{
+    /**
+     * Validate and create the given project.
+     *
+     * @param  array  $input
+     */
+    public function __invoke(array $input): Project
+    {
+        $validated = Validator::make($input, [
+            'title' => ['required', 'string', 'min:1', 'max:255', 'unique:projects,title'],
+            'type' => ['required', new Enum(ProjectType::class)],
+            'topics' => ['nullable', 'array'],
+            'topics.*' => ['string', 'min:1', 'max:255'],
+            'countries' => ['nullable', 'array'],
+            'countries.*' => ['string', 'max:3', new Enum(CountryAlpha3::class)],
+            'organizations' => ['array'],
+            'organizations.*' => ['string', 'min:1', 'max:255'],
+            'properties' => ['nullable', 'array'],
+            'slug' => ['nullable', 'string', 'min:1', 'max:255', 'unique:projects,slug'],
+            'description' => ['nullable', 'string'],
+            'starts_at' => ['nullable', 'date'],
+            'ends_at' => ['nullable', 'date', 'after:starts_at'],
+        ])->validate();
+
+        return Project::create([
+            'title' => $validated['title'],
+            'type' => $validated['type'],
+            'topics' => $validated['topics'],
+            'countries' => $validated['countries'],
+            'organizations' => $validated['organizations'],
+            'properties' => $validated['properties'] ?? [],
+            'slug' => $validated['slug'] ?? str($validated['title'])->substr(0, 240)->slug()->toString(),
+            'description' => $validated['description'] ?? null,
+            'starts_at' => $validated['starts_at'] ?? null,
+            'ends_at' => $validated['ends_at'] ?? null,
+        ]);
+
+    }
+}
