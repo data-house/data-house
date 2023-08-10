4f3ce716e18e329e930b01a24c6a11a1044f69df
Alessio Vertemati <alessio.vertemati@gmail.com>
2023-08-10 18:13:20 +0200

    Ensure setting not null

--- resources/views/components/visualization-style-switcher.blade.php
+++ resources/views/components/visualization-style-switcher.blade.php
@@ -18,9 +18,9 @@
         <a href="{{ route('user-preferences', [
             'preference' => 'VISUALIZATION_LAYOUT',
             'value' => $style
-        ])}}" class="p-1 rounded ring-1 hover:ring-indigo-600 hover:bg-indigo-100 focus:outline-none focus:ring-indigo-600 focus:bg-indigo-100 {{ (is_null($visualizationPreference) && $style==='grid') || $visualizationPreference->value === $style ? 'ring-indigo-400 text-indigo-600 bg-indigo-50' : ' ring-stone-400 '}}" title="{{ __('Change layout to :layout', ['layout' => $style]) }}">
+        ])}}" class="p-1 rounded ring-1 hover:ring-indigo-600 hover:bg-indigo-100 focus:outline-none focus:ring-indigo-600 focus:bg-indigo-100 {{ (is_null($visualizationPreference) && $style==='grid') || (!is_null($visualizationPreference) && $visualizationPreference->value === $style) ? 'ring-indigo-400 text-indigo-600 bg-indigo-50' : ' ring-stone-400 '}}" title="{{ __('Change layout to :layout', ['layout' => $style]) }}">
         
-            <x-dynamic-component :component="(is_null($visualizationPreference) && $style==='grid') || $visualizationPreference->value === $style ? 'heroicon-s-'.$icons[$style] : 'heroicon-o-'.$icons[$style]" class="w-6 h-6" />
+            <x-dynamic-component :component="(is_null($visualizationPreference) && $style==='grid') || (!is_null($visualizationPreference) && $visualizationPreference->value === $style) ? 'heroicon-s-'.$icons[$style] : 'heroicon-o-'.$icons[$style]" class="w-6 h-6" />
 
         </a>
     @endforeach
