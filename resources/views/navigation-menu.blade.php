ad0812a4f02b04c86c75d800b381c22ac3beedca
Alessio Vertemati <alessio.vertemati@gmail.com>
2023-09-21 21:14:17 +0200

    Restore dashboard button

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,9 +12,9 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    {{-- <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
-                    </x-nav-link> --}}
+                    </x-nav-link>
                     <x-nav-link href="{{ route('documents.library') }}" :active="request()->routeIs('documents.*') || request()->routeIs('imports.*') || request()->routeIs('questions.*') || request()->routeIs('collections.*')">
                         {{ __('Digital Library') }}
                     </x-nav-link>
