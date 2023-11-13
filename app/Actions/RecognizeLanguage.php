287afe19bd1561d7a41de7bf318ad0edcc0c81ee
Alessio Vertemati <alessio.vertemati@gmail.com>
2023-11-13 19:04:15 +0100

    Recognize document content language

---
+++ app/Actions/RecognizeLanguage.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\Document;
+use App\Models\DocumentType;
+use Illuminate\Support\Collection;
+use Oneofftech\LaravelLanguageRecognizer\Support\Facades\LanguageRecognizer;
+use PrinsFrank\Standards\Language\LanguageAlpha3Common;
+use PrinsFrank\Standards\Language\LanguageAlpha3Terminology;
+
+class RecognizeLanguage
+{
+    protected const ALLOWED_LANGUAGES = [
+        // TODO: Needs to be revised, this is a preliminary implementation as we only target those languages.
+        LanguageAlpha3Common::English,
+        LanguageAlpha3Common::Italian,
+        LanguageAlpha3Common::Spanish_Castilian,
+        LanguageAlpha3Terminology::French,
+        LanguageAlpha3Terminology::German,
+        LanguageAlpha3Common::Russian,
+    ];
+
+    protected const MINIMUM_CONFIDENCE = 0.8;
+
+    /**
+     * Attempt to recognize the most probable language of the documen's content
+     *
+     * @param  \App\Models\Document  $document
+     */
+    public function __invoke(Document $document): ?Collection
+    {
+        $possibleLanguages = collect(LanguageRecognizer::recognize($document->getContent()->all()));
+
+        // We check if the current recognized languages are within the list of acceptable ones
+        // and we keep only the ones above a certain threshold
+        // As a workaround on the current meaning of the languages field we transform
+        // the result in a collection of alpha-2 iso codes
+
+        return $possibleLanguages
+            ->only(collect(self::ALLOWED_LANGUAGES)->map->value)
+            ->map(function($confidence, $code){
+
+                if($confidence < self::MINIMUM_CONFIDENCE){
+                    return null;
+                }
+                
+                $lang = LanguageAlpha3Common::tryFrom($code) ?? LanguageAlpha3Terminology::tryFrom($code);
+
+                return [
+                    'alpha2' => $lang->toLanguageAlpha2(),
+                    'alpha3' => $code,
+                    'confidence' => $confidence,
+                ];
+            })
+            ->values()
+            ->filter()
+            ->sortByDesc('confidence')
+            ->pluck('alpha2');
+    }
+
+}
