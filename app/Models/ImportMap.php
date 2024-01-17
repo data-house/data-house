7520729040d2b958ae8eeb1ce61a386241b5b579
Alessio <avvertix@users.noreply.github.com>
2024-01-17 18:57:33 +0100

    Allow to set document visibility during import mapping creation (#72)

--- app/Models/ImportMap.php
+++ app/Models/ImportMap.php
@@ -34,15 +34,15 @@ class ImportMap extends Model
         'mapped_uploader',
         'recursive',
         'filters',
+        'visibility',
     ];
 
     protected $casts = [
         'recursive' => 'boolean',
         'filters' => 'json',
         'status' => ImportStatus::class,
+        'visibility' => Visibility::class,
     ];
-
-
     
     /**
      * Get the columns that should receive a unique identifier.
