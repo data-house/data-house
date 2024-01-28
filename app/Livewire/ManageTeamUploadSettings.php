c0ffa1f467645fa761bda85c6bc7c098832d1095
Alessio Vertemati <alessio.vertemati@gmail.com>
2024-01-28 18:53:18 +0100

    Allow to manage upload settings within team config

---
+++ app/Livewire/ManageTeamUploadSettings.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Data\UploadSettings;
+use Livewire\Component;
+
+class ManageTeamUploadSettings extends Component
+{
+    /**
+     * The team instance.
+     *
+     * @var mixed
+     */
+    public $team;
+
+    /**
+     * The "upload settings" form state.
+     *
+     * @var array
+     */
+    public $uploadSettingsForm = [
+        'uploadLinkUrl' => '',
+        'supportProjects' => false,
+    ];
+
+
+    /**
+     * Mount the component.
+     *
+     * @param  mixed  $team
+     * @return void
+     */
+    public function mount($team)
+    {
+        $this->team = $team;
+        if($team->settings?->upload){
+            $this->uploadSettingsForm = $team->settings->upload->toArray();
+        }
+    }
+
+    /**
+     * Add a new team member to a team.
+     *
+     * @return void
+     */
+    public function updateUploadSettings()
+    {
+        $this->resetErrorBag();
+
+        $this->validate([
+            'uploadSettingsForm.uploadLinkUrl' => 'required|url|max:250',
+            'uploadSettingsForm.supportProjects' => 'bool',
+        ]);
+
+        $this->team->settings->upload = UploadSettings::from($this->uploadSettingsForm);
+
+        $this->team->save();
+
+        $this->dispatch('saved');
+    }
+
+
+    public function render()
+    {
+        return view('livewire.manage-team-upload-settings');
+    }
+}
---
+++ resources/views/livewire/manage-team-upload-settings.blade.php
@@ -0,0 +1,53 @@
+<div>
+    <x-section-border />
+    
+    <div class="mt-10 sm:mt-0">
+        <!-- Upload settings -->
+        <x-form-section submit="updateUploadSettings">
+            <x-slot name="title">
+                {{ __('Upload Link') }}
+            </x-slot>
+    
+            <x-slot name="description">
+                {{ __('Configure file upload by using external shares with a link.') }}
+            </x-slot>
+    
+            <x-slot name="form">
+                <div class="col-span-6">
+                    <div class="max-w-xl text-sm text-stone-600">
+                        {{ __('Please provide the URL of the shared folder to redirect the users to.') }}
+                    </div>
+                </div>
+    
+                <div class="col-span-6 sm:col-span-4">
+                    <x-label for="uploadLink" value="{{ __('Url') }}" />
+                    <x-input id="uploadLink" type="text" class="mt-1 block w-full" wire:model="uploadSettingsForm.uploadLinkUrl" />
+                    <x-input-error for="uploadSettingsForm.uploadLinkUrl" class="mt-2" />
+                </div>
+    
+                <div class="col-span-6 sm:col-span-4">
+                    <x-label for="supportProjects" value="{{ __('Support upload in projects') }}" />
+
+                    <label for="supportProjects" class="mt-1 flex items-center">
+                        <x-checkbox id="supportProjects" value="1" wire:model="uploadSettingsForm.supportProjects" />
+                        <span class="ml-2 text-sm text-stone-600">{{ __('Allow upload within projects') }}</span>
+                    </label>
+
+                    <x-input-error for="uploadSettingsForm.supportProjects" class="mt-2" />
+                </div>
+    
+    
+            </x-slot>
+    
+            <x-slot name="actions">
+                <x-action-message class="mr-3" on="saved">
+                    {{ __('Configuration saved.') }}
+                </x-action-message>
+    
+                <x-button>
+                    {{ __('Save') }}
+                </x-button>
+            </x-slot>
+        </x-form-section>
+    </div>
+</div>
