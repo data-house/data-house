cb2baaa363e0185be98089fe8ebd40e8955f7306
Alessio <alessio.vertemati@gmail.com>
2024-02-17 20:53:26 +0100

    button variants

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -1,3 +1,3 @@
-<a {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 bg-stone-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-stone-700 focus:bg-stone-700 active:bg-stone-900 focus:outline-none focus:ring-2 focus:ring-lime-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+<a {{ $attributes->merge(['class' => 'inline-flex items-center gap-1 px-4 py-2 bg-stone-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-stone-700 focus:bg-stone-700 active:bg-stone-900 focus:outline-none focus:ring-2 focus:ring-lime-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </a>
