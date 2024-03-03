bbc85344e1d04f2331fab2bf434659700cefb830
Alessio <alessio.vertemati@gmail.com>
2024-03-03 09:59:26 +0100

    Add readable message for expired csrf token

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,9 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
+use Illuminate\Session\TokenMismatchException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -43,6 +46,22 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+        
+
+        $this->renderable(function (HttpException $e, Request $request) {
+
+            if($e->getStatusCode() === 419 || ($e->getPrevious() && $e->getPrevious() instanceof TokenMismatchException)){
+                
+                return redirect()
+                    ->back()
+                    ->withInput()
+                    ->with('status', __('Looks like your session expired. Please try refreshing the page and submitting your request again.'))
+                    ->with('flash', [
+                        'bannerStyle' => 'warning',
+                        'banner' => __('Looks like your session expired. Please try refreshing the page and submitting your request again.'),
+                    ]);
+            }
         });
     }
 }
