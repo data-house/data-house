92faf00a2a0717c3157ae4cbe8291a298abfe373
Alessio <alessio.vertemati@gmail.com>
2024-03-31 21:28:34 +0200

    Basic user listing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminProjectController;
+use App\Http\Controllers\Admin\AdminUserController;
 use App\Http\Controllers\Admin\InstanceOverviewController;
 use App\Http\Controllers\CollectionController;
 use App\Http\Controllers\CreateMultipleQuestionController;
@@ -86,7 +87,7 @@ Route::middleware([
 ->group(function () {
     Route::get('/', InstanceOverviewController::class)->name('dashboard');
     
-    Route::get('/users', InstanceOverviewController::class)->name('users.index');
+    Route::resource('/users', AdminUserController::class)->only(['index']);
     
     Route::resource('/projects', AdminProjectController::class)->only(['index']);
     
