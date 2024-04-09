769e6b0f976566ffcf01138499c34fe5eb45d947
Alessio <alessio.vertemati@gmail.com>
2024-04-09 21:29:34 +0200

    Project topics

--- resources/views/components/project-card.blade.php
+++ resources/views/components/project-card.blade.php
@@ -19,7 +19,7 @@
             <div class="flex flex-wrap gap-2">
                 @foreach ($project->formattedTopics() as $topic)
                     <a title="{{ __('Explore documents connected to projects in :value', ['value' => $topic['name']]) }}"
-                    href="{{ route('documents.library', ['topics' => [$topic['id']]])}}"
+                    href="{{ route('documents.library', ['project_topics' => [$topic['id']]])}}"
                     class="inline-flex gap-1 items-center text-xs px-2 py-1 rounded-xl bg-gray-200 text-gray-900 hover:bg-indigo-200 focus:bg-indigo-200 hover:text-indigo-800 focus:text-indigo-800 group">
                         <x-heroicon-o-hashtag class="w-3 h-3 text-gray-700 group-hover:text-indigo-600" />
                         {{ $topic['name'] }}
