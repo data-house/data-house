1f06a409140909c92e8e247ee519b7ece46aa0b4
Alessio Vertemati <alessio.vertemati@gmail.com>
2024-05-22 14:53:48 +0200

    Update summary action

---
+++ app/Actions/Summary/UpdateSummary.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Actions\Summary;
+
+use App\Models\Document;
+use App\Models\DocumentSummary;
+use App\Models\User;
+use PrinsFrank\Standards\Language\LanguageAlpha2;
+
+class UpdateSummary
+{
+    /**
+     * Update an existing document summary
+     *
+     * @param  \App\Models\DocumentSummary  $summary
+     */
+    public function __invoke(DocumentSummary $summary, string $text, ?LanguageAlpha2 $language = null, User $user = null): DocumentSummary
+    {
+        if($summary->isAiGenerated() || $summary->user_id !== $user?->getKey()){
+            return (new SaveSummary())($summary->document, $text, $language, $user);
+        }
+
+        $summary->text = $text;
+        $summary->language = $language;
+
+        $summary->save();
+
+        return $summary->fresh();
+    }
+
+}
