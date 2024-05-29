91bc75a41e01f15947a23ac05c29877f3ba8ca48
Alessio Vertemati <alessio.vertemati@gmail.com>
2024-05-29 21:21:44 +0200

    Add tooltip

--- resources/views/livewire/notifications/notification-bell.blade.php
+++ resources/views/livewire/notifications/notification-bell.blade.php
@@ -4,13 +4,14 @@
         relative flex items-center border-2 p-1 border-transparent rounded-full focus:outline-none focus:border-stone-300 transition
         @if ($this->hasUnreadNotifications)  px-2 py-1 bg-orange-50 text-orange-800 ring-orange-600/20 ring-1 ring-inset @endif
     "
->
+    x-tooltip.raw="{{ $this->hasUnreadNotifications ? __('You have unread notifications') : __('Notifications') }}"
+    >
+    
     
     @if ($this->hasUnreadNotifications)
         <span wire:transition.origin.right class="inline-flex items-center  px-2 text-xs font-mono font-medium ">
             {{ \Illuminate\Support\Number::abbreviate($this->unreadNotificationsCount) }}
         </span>
-    
         <x-heroicon-o-bell-alert class="size-5 " />
     @else
         <x-heroicon-o-bell class="size-5" />
