a1369d87830f4f0b35de1b5d255b44f3c0217b6f
Alessio Vertemati <alessio.vertemati@gmail.com>
2024-06-04 13:59:04 +0200

    Fix notification localization missing

---
+++ lang/en/notification-types.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Notification Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default messages used to 
+    | provide the title of the notification based on the type 
+    | of the notifications
+    |
+    */
+
+    'notification' => [
+        'password-changed' => 'Your account password changed',
+        'import-failed' => 'Import mapping processing failed',
+    ],
+
+];
