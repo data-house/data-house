6df5def0123aa19fe0f06132f0b850cbb933250b
Alessio Vertemati <alessio.vertemati@gmail.com>
2025-03-11 16:43:43 +0100

    Remove double navigation

--- resources/views/library-navigation-menu.blade.php
+++ resources/views/library-navigation-menu.blade.php
@@ -1,11 +1,5 @@
 @question()
 @feature(Flag::questionWithAI())
-    <x-heading-nav-link href="{{ route('documents.library') }}" :active="request()->routeIs('documents.*')">{{ __('Documents') }}</x-heading-nav-link>
-
-    @can('viewAny', \App\Models\Question::class)
-        <x-heading-nav-link href="{{ route('questions.index') }}" :active="request()->routeIs('questions.*')">{{ __('Questions') }}</x-heading-nav-link>
-    @endcan
-    
     @can('viewAny', \App\Models\QuestionReview::class)
         <x-heading-nav-link href="{{ route('question-reviews.index') }}" :active="request()->routeIs('question-reviews.*')">{{ __('Question Reviews') }}</x-heading-nav-link>
     @endcan
