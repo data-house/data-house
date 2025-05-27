276ef69b119284c741289e9699f3fdde946ac17b
Alessio Vertemati <alessio.vertemati@gmail.com>
2025-05-27 16:01:54 +0200

    Add Filament forms and tables dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,18 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 
+import filamentPreset from './vendor/filament/support/tailwind.config.preset';
+
 /** @type {import('tailwindcss').Config} */
 export default {
+    presets: [filamentPreset],
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
+        './app/Filament/**/*.php',
+        './resources/views/filament/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
         './resources/views/**/*.blade.php',
     ],
 
